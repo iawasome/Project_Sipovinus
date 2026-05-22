aea4def7e867e4ae0a07937e187290cd3774b10f
iawasome <fajarcool234@gmail.com>
2026-05-22 08:10:20 +0700

    Fix loop redirect: trust all proxies for cloud HTTPS alignment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // SAKLAR UTAMA: Membuka sumbatan HTTPS Reverse Proxy di Cloud Server
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
